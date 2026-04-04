a496e86e684bac51944ebe9494ceb32a0ed1db6a
ragulpanneerselvam@gmail.com <ragulpanneerselvam@gmail.com>
2026-04-04 11:47:04 +0530

    Section 2

---
+++ Persistence/AppDbContext.cs
@@ -0,0 +1,13 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+     public DbSet<Domain.Activity> Activities { get; set; }
+}
